26f776fde3601323e6c744cfdddefc83bddddea3
DrackDMG <drackmg@gmail.com>
2025-09-26 16:44:48 -0600

    test evaluar que exista router outlet

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,38 @@
-import { provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { Component, provideZonelessChangeDetection } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let compiled: HTMLElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
       providers: [provideZonelessChangeDetection()]
     }).compileComponents();
+    fixture = TestBed.createComponent(App);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
-  });
+  it("should be 3", () => {
+    const num1 = 1;
+    const num2 = 2;
+    expect(num1 + num2).toBe(3);
+  })
+
+   it('should render title', () => {
+     const app = fixture.componentInstance;
+     console.log(compiled);
+     expect(app.title()).toEqual('Hello, zoneless-calculator');
+   });
+
+   it('should render routerOutlet', () => {
+    //expect(compiled.querySelector('router-outlet')).not.toBeNull();
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+   })
 });
