53123565247eb343ec3016c36bc5a172c51916e9
DrackDMG <drackmg@gmail.com>
2025-09-29 22:05:54 -0600

    simular key pressed

--- src/app/calculator/components/calculator/calculator.component.spec.ts
+++ src/app/calculator/components/calculator/calculator.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorComponent } from './calculator.component';
 import { CalculatorService } from '@/calculator/services/calculator.service';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
 
 class MockCalculatorService {
   public resulText = jasmine.createSpy('resulText').and.returnValue('1000.00');
@@ -48,11 +50,44 @@ describe('CalculatorComponent', () => {
   it('should display proper calculator values', () => {
     mockCalculatorService.resulText = jasmine.createSpy('resulText').and.returnValue('10.00');
     fixture.detectChanges();
-    console.log(compiled.querySelector('span')?.innerText);
+    //console.log(compiled.querySelector('span')?.innerText);
 
     expect(compiled.querySelector('span')?.innerText).toBe('5000.00 +');
     expect(component.resultText()).toBe('10.00'); // 👈 actualizo la expectativa al nuevo valor
     expect(component.subResultText()).toBe('5000.00');
     expect(component.lastOperator()).toBe('+');
   });
+
+  it('should have 19 calculator buttons', () => {
+    expect(component.calculatorButtons().length).toBe(19);
+  });
+
+  it('should have 19 calculator buttons with content projection', () => {
+    const buttons = compiled.querySelectorAll('calculator-button');
+    //const buttonByDirective = fixture.debugElement.queryAll(By.directive(CalculatorButtonComponent));
+
+    expect(buttons.length).toBe(19);
+
+    expect(buttons[0].textContent).toBe('C');
+    expect(buttons[1].textContent).toBe('+/-');
+    expect(buttons[2].textContent).toBe('%');
+    expect(buttons[3].textContent).toBe('÷');
+  });
+
+  it('should handle keyboard events correctly', () => {
+    const eventEnter = new KeyboardEvent('keyup', { key: 'Enter' });
+    document.dispatchEvent(eventEnter);
+
+    expect(mockCalculatorService.constructorNumber).toHaveBeenCalledWith('=');
+  })
+
+  it('should display result text correctly', () => {
+    mockCalculatorService.resulText.and.returnValue('123');
+    mockCalculatorService.subResulText.and.returnValue('456');
+    mockCalculatorService.lastOperator.and.returnValue('x');
+    fixture.detectChanges();
+    expect(component.resultText()).toBe('123');
+    expect(compiled.querySelector('#sub-result')?.textContent).toContain('456 x');
+  });
+
 });
